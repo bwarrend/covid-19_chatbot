1a191bbd0c2fe1b71c61b8e6dc9fd7b3b61dcbb4
Brett W <bwarrend@my.hpu.edu>
2020-11-10 19:53:44 -1000

    gh-pages

---
+++ precache-manifest.224b77264f91250d43523530ae90d1ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3049127df069ad301b5f",
+    "url": "css/app.5fc3e8b7.css"
+  },
+  {
+    "revision": "8f2da8bccf7d25bd1f8111a5e2397eff",
+    "url": "fonts/MaterialIcons-Round.8f2da8bc.woff2"
+  },
+  {
+    "revision": "b8ef0b56e5ac8c6e1d1b937e2f78f821",
+    "url": "fonts/MaterialIcons-Round.b8ef0b56.woff"
+  },
+  {
+    "revision": "9549360090baf2eb8b25d3a9708fc19d",
+    "url": "fonts/Roboto-Regular.95493600.woff2"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "fonts/Roboto-Regular.a91ad097.woff"
+  },
+  {
+    "revision": "a5e7532c37c71ecbc31be671b38c1293",
+    "url": "img/bubble-left.a5e7532c.svg"
+  },
+  {
+    "revision": "6ee1b2165d8473a18fb2e3154c719e79",
+    "url": "img/bubble-left_dark.6ee1b216.svg"
+  },
+  {
+    "revision": "8d9d93e860009d5cbfbcc21268a8f4f8",
+    "url": "img/bubble-right.8d9d93e8.svg"
+  },
+  {
+    "revision": "979cd115e336808113234e6b4b772857",
+    "url": "img/bubble-right_dark.979cd115.svg"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "96b924aa3aad0bc4febddf3f5b7ac900",
+    "url": "index.html"
+  },
+  {
+    "revision": "3049127df069ad301b5f",
+    "url": "js/app.9f8ec44d.js"
+  },
+  {
+    "revision": "422accad23606519165f",
+    "url": "js/chunk-vendors.9f66c727.js"
+  },
+  {
+    "revision": "bfae9faebbd4ca00d77737a88e057227",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
